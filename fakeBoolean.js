a1d1de3773776c9750d6867c3aa5e86360370431
pazinmd <95488500+pazinmd@users.noreply.github.com>
2022-05-31 21:15:50 +0200

    Update fakeBoolean.js

--- fakeBoolean.js
+++ fakeBoolean.js
@@ -10,3 +10,13 @@ result = result.concat("0"); } //working with new string using a concat method
 }
   return result;
   }
+
+
+// a bit shorter version
+function fakeBin(x){
+  let result = '';
+for (let i = 0; i < x.length; i++) {
+ x[i] < 5 ?  result = result.concat("0") : result =  result.concat("1"); 
+}
+  return result;
+  }
